7b0c5b151b7e1f109dcded49de0c10cb9404719c
LaraMartincevic <laramartincevic88@gmail.com>
2022-09-06 18:54:39 +0200

    delete nepotrebno

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -97,9 +97,6 @@ export default {
           console.log(response);
           if (response.data) {
             console.log(response.data);
-            // this.$router.push({
-            //   name: "test",
-            // });
           }
         });
     },
